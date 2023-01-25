22d451f3e2c6dd884bc24e294d761dced5fc3b10
amauriciogonzalez <88101535+amauriciogonzalez@users.noreply.github.com>
2023-01-25 17:45:28 -0500

    Time: 197 ms (33.22%), Space: 19.6 MB (59.72%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        # Merging with Divide and Conquer
+
+        if not lists:
+            return None
+
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.merge2Lists(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+
+
+    def merge2Lists(self, l1, l2):
+        prevHead = ListNode()
+        temp = prevHead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        if l1:
+            temp.next = l1
+        if l2:
+            temp.next = l2
+        return prevHead.next
+            
+
+            
